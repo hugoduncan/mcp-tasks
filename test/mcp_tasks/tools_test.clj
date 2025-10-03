57590977c86b9e692746d6caf909791d2fa4916c
Hugo Duncan <hugo@hugoduncan.org>
2025-10-03 09:37:46 -0400

    chore: add cljstyle config and reformat (#7)

--- test/mcp_tasks/tools_test.clj
+++ test/mcp_tasks/tools_test.clj
@@ -1,33 +1,38 @@
 (ns mcp-tasks.tools-test
   (:require
-   [clojure.java.io :as io]
-   [clojure.string :as str]
-   [clojure.test :refer [deftest is testing]]
-   [mcp-tasks.tools :as sut]))
+    [clojure.java.io :as io]
+    [clojure.string :as str]
+    [clojure.test :refer [deftest is testing]]
+    [mcp-tasks.tools :as sut]))
 
 (def ^:private test-fixtures-dir "test-resources/tools-test")
 
-(defn- cleanup-test-fixtures []
+(defn- cleanup-test-fixtures
+  []
   (let [dir (io/file test-fixtures-dir)]
     (when (.exists dir)
       (doseq [file (reverse (file-seq dir))]
         (.delete file)))))
 
-(defn- setup-test-dir []
+(defn- setup-test-dir
+  []
   (cleanup-test-fixtures)
   (.mkdirs (io/file test-fixtures-dir "tasks"))
   (.mkdirs (io/file test-fixtures-dir "complete")))
 
-(defn- write-test-file [path content]
+(defn- write-test-file
+  [path content]
   (spit (str test-fixtures-dir "/" path) content))
 
-(defn- read-test-file [path]
+(defn- read-test-file
+  [path]
   (let [file (io/file (str test-fixtures-dir "/" path))]
     (if (.exists file)
       (slurp file)
       "")))
 
-(defn- with-test-files [f]
+(defn- with-test-files
+  [f]
   (with-redefs [sut/read-task-file
                 (fn [path]
                   (read-test-file (str/replace path #"^\.mcp-tasks/" "")))
@@ -45,8 +50,8 @@
                    "- [ ] first task\n      detail line\n- [ ] second task")
   (with-test-files
     #(let [result (sut/complete-task-impl
-                   {:category "test"
-                    :task-text "first task"})]
+                    {:category "test"
+                     :task-text "first task"})]
        (is (false? (:isError result)))
        (is (= "- [x] first task\n      detail line"
               (read-test-file "complete/test.md")))
@@ -60,9 +65,9 @@
   (write-test-file "tasks/test.md" "- [ ] task with comment")
   (with-test-files
     #(let [result (sut/complete-task-impl
-                   {:category "test"
-                    :task-text "task with comment"
-                    :completion-comment "Added feature X"})]
+                    {:category "test"
+                     :task-text "task with comment"
+                     :completion-comment "Added feature X"})]
        (is (false? (:isError result)))
        (is (= "- [x] task with comment\n\nAdded feature X"
               (read-test-file "complete/test.md")))))
@@ -75,8 +80,8 @@
   (write-test-file "complete/test.md" "- [x] old task")
   (with-test-files
     #(let [result (sut/complete-task-impl
-                   {:category "test"
-                    :task-text "new task"})]
+                    {:category "test"
+                     :task-text "new task"})]
        (is (false? (:isError result)))
        (is (= "- [x] old task\n- [x] new task"
               (read-test-file "complete/test.md")))))
@@ -89,8 +94,8 @@
   (write-test-file "tasks/test.md" "- [ ] only task")
   (with-test-files
     #(let [result (sut/complete-task-impl
-                   {:category "test"
-                    :task-text "only task"})]
+                    {:category "test"
+                     :task-text "only task"})]
        (is (false? (:isError result)))
        (is (= "" (read-test-file "tasks/test.md")))))
   (cleanup-test-fixtures))
@@ -102,8 +107,8 @@
   (write-test-file "tasks/test.md" "- [ ] actual task")
   (with-test-files
     #(let [result (sut/complete-task-impl
-                   {:category "test"
-                    :task-text "wrong task"})]
+                    {:category "test"
+                     :task-text "wrong task"})]
        (is (true? (:isError result)))
        (is (re-find #"does not match"
                     (get-in result [:content 0 :text])))))
@@ -116,8 +121,8 @@
   (write-test-file "tasks/test.md" "")
   (with-test-files
     #(let [result (sut/complete-task-impl
-                   {:category "test"
-                    :task-text "any task"})]
+                    {:category "test"
+                     :task-text "any task"})]
        (is (true? (:isError result)))
        (is (re-find #"No tasks found"
                     (get-in result [:content 0 :text])))))
@@ -129,8 +134,8 @@
   (write-test-file "tasks/test.md" "- [ ] Task With Capitals")
   (with-test-files
     #(let [result (sut/complete-task-impl
-                   {:category "test"
-                    :task-text "task with capitals"})]
+                    {:category "test"
+                     :task-text "task with capitals"})]
        (is (false? (:isError result)))))
   (cleanup-test-fixtures))
 
@@ -142,8 +147,8 @@
                    "- [ ] first task\n      line 2\n      line 3\n- [ ] second task")
   (with-test-files
     #(let [result (sut/complete-task-impl
-                   {:category "test"
-                    :task-text "first task"})]
+                    {:category "test"
+                     :task-text "first task"})]
        (is (false? (:isError result)))
        (is (= "- [x] first task\n      line 2\n      line 3"
               (read-test-file "complete/test.md")))
