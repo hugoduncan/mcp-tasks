2e580ce3a89353cb5c558d23aac4e14266bd3824
Hugo Duncan <hugo@hugoduncan.org>
2025-10-25 09:21:03 -0400

    debug: add comprehensive git state logging to diagnose test failure

--- test/mcp_tasks/git_sync_integration_test.clj
+++ test/mcp_tasks/git_sync_integration_test.clj
@@ -96,6 +96,8 @@
           ;; Set pull.ff=only for consistent test behavior across git versions
           ;; (uses local git config - doesn't affect developer environment)
           (sh/sh "git" "config" "pull.ff" "only" :dir tasks-dir)
+          ;; Ensure we're on master branch
+          (sh/sh "git" "checkout" "-b" "master" :dir tasks-dir)
           (h/write-ednl-test-file test-dir "tasks.ednl"
                                   [{:id 1 :parent-id nil :title "Initial Task" :description "" :design ""
                                     :category "simple" :type :task :status :open :meta {} :relations []}])
@@ -105,21 +107,34 @@
           ;; Create a "remote" by cloning
           (let [remote-dir (str test-dir "-remote")
                 _ (sh/sh "git" "clone" tasks-dir remote-dir)
-                _ (sh/sh "git" "remote" "add" "origin" remote-dir :dir tasks-dir)]
+                ;; Ensure clone is on master branch
+                _ (sh/sh "git" "checkout" "master" :dir remote-dir)
+                _ (sh/sh "git" "remote" "add" "origin" remote-dir :dir tasks-dir)
+                ;; Verify git log shows commit
+                log-before (sh/sh "git" "log" "--oneline" "-1" :dir tasks-dir)
+                _ (println "DEBUG: Git log before divergence (original):" log-before)]
 
             ;; Make conflicting change in original - modify the SAME task
             (h/write-ednl-test-file test-dir "tasks.ednl"
                                     [{:id 1 :parent-id nil :title "Modified in Original" :description "Changed A" :design ""
                                       :category "simple" :type :task :status :open :meta {} :relations []}])
             (sh/sh "git" "add" "." :dir tasks-dir)
-            (sh/sh "git" "commit" "-m" "Modify task in original" :dir tasks-dir)
+            (let [commit-result (sh/sh "git" "commit" "-m" "Modify task in original" :dir tasks-dir)]
+              (println "DEBUG: Commit in original:" commit-result))
 
             ;; Make DIFFERENT conflicting change in remote - modify the SAME task differently
             (h/write-ednl-test-file (str test-dir "-remote") "tasks.ednl"
                                     [{:id 1 :parent-id nil :title "Modified in Remote" :description "Changed B" :design ""
                                       :category "simple" :type :task :status :open :meta {} :relations []}])
             (sh/sh "git" "add" "." :dir remote-dir)
-            (sh/sh "git" "commit" "-m" "Modify task in remote" :dir remote-dir)
+            (let [commit-result (sh/sh "git" "commit" "-m" "Modify task in remote" :dir remote-dir)]
+              (println "DEBUG: Commit in remote:" commit-result))
+
+            ;; Verify branches have diverged
+            (let [log-original (sh/sh "git" "log" "--oneline" "-2" :dir tasks-dir)
+                  log-remote (sh/sh "git" "log" "--oneline" "-2" :dir remote-dir)]
+              (println "DEBUG: Log original (2 commits):" log-original)
+              (println "DEBUG: Log remote (2 commits):" log-remote))
 
             ;; Now try to sync in original - should detect divergent histories
             ;; and fail when attempting to merge
@@ -135,7 +150,7 @@
                   (is (contains? result :error))
                   (is (contains? result :error-type)))
 
-                ;; If with-task-lock wraps it in tool error response  
+                ;; If with-task-lock wraps it in tool error response
                 (and (map? result) (:isError result))
                 (is (string? (-> result :content first :text)))
 
